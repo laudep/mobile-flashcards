b9e30f1b0fad61f14580d37fee369d7dbf78ca0b
Laurens Deprost <ldeprost@gmail.com>
2019-03-04 15:41:11 +0100

    feat: Add basic placeholder components

---
+++ components/NewQuestion.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class NewQuestion extends Component {
+  render() {
+    return (
+      <View>
+        <Text>New Question</Text>
+      </View>
+    );
+  }
+}
+
+export default NewQuestion;
---
+++ components/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class Results extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Results</Text>
+      </View>
+    );
+  }
+}
+
+export default Results;
